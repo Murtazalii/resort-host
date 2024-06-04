6194e6570c237de518aef1635faaabc505dcd56e
BrianRicci <yunus.ibragimov49@gmail.com>
2024-06-04 12:20:55 +0300

    init commit

---
+++ js/summ.js
@@ -0,0 +1,30 @@
+let obj = {
+    usluga1: 1740,
+    usluga2: 1280,
+    usluga3: 2260,
+    usluga4: 1970,
+    usluga5: 4630,
+    usluga6: 2670,
+    usluga7: 3300,
+    usluga8: 2900,
+    usluga9: 2670,
+    usluga10: 5510,
+    usluga11: 4410,
+    usluga12: 1280,
+  }
+  
+  let result = document.getElementById('result');
+  let services = document.querySelectorAll('.services');
+  let sum = 0; // Сумма выбранных
+  
+  for( let i = 0; i < services.length; i++ ){
+    let checked = false; // Все кнопки изначально не кликнуты
+  
+    services[i].addEventListener('click', function(){
+      this.classList.toggle('active');
+      checked = !checked; // false ←→ true - постоянное переключение при клике
+      let val = obj[ this.dataset.name ]; // Сразу достается нужное число
+      sum = checked ? sum + val : sum - val;
+      result.textContent = sum;
+    });
+  }
